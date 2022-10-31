d488b720f3cbdf1f9cef56525707b94ec1cebe2f
analuzma <92894639+analuzma@users.noreply.github.com>
2022-10-31 10:06:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-cjnkyi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-cjnkyi)
---
+++ index.ts
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Question 1
+
+// Question 2
+
+// Question 3
+
+// Question 4
