a8150816ef215be0b7fd596e9a744f0294895160
analuzma <92894639+analuzma@users.noreply.github.com>
2022-10-31 11:56:36 -0700

    3 files modified

--- index.ts
+++ index.ts
@@ -2,9 +2,46 @@
 import './style.css';
 
 // Question 1
+function countClick() {
+  let count = 0;
+  let counter = document.getElementById('question-one'); //button
+  let resultCount = document.getElementById('click-counter'); //result
+
+  counter.addEventListener('click', () => {
+    count += 1;
+    resultCount.innerHTML = count;
+  });
+  countClick();
+}
 
 // Question 2
+function removeEvenPClass() {
+  let allEvenP = document.querySelectorAll('.even-p'); //select all class even-p
+  let removeClassBtn = document.getElementById('question-two'); //button
+  removeClassBtn.addEventListener('click', () => {
+    for (i = 0; i < allEvenP.length; i++) {
+      allEvenP[i].classList.remove('even-p');
+    }
+  });
+}
+removeEvenPClass();
 
 // Question 3
 
 // Question 4
+async function helloWorld() {
+  const promiseFunction = await myPromise();
+  let promiseResponse = document.getElementById('question-four-p'); //answer must be in inner html
+  promiseResponse.innerHTML = 'Hello World!';
+}
+function myPromise() {
+  let promiseBtn = document.getElementById('question-four');
+  promiseBtn.addEventListener('click', () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(helloWorld());
+      }, 3000);
+    });
+  });
+}
+myPromise();
